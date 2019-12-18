0df8843818bea8fa677595aab6015ed4382cda2f
Colm McSharry <colmmcsharry@hotmail.com>
2019-12-18 19:25:24 +0000

    welcome done

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,17 @@ import ReactDOM from "react-dom";
 function Welcome ()
 {
 	return(
-		<p1>Hello and welcome to networkers, click the logo to begin!</p1>
+		<div className="Fader"> <span style={{fontSize:'40px', fontFamily: 'itim'}}> Hello and welcome to NetWorkers.</span>
+		<br/><br/>
+			Need some renovations done on your house? 
+		<br/><br/>
+			Need a clown to entertain your children? 
+		<br/>	<br/>
+		Browse our listings and find the worker you need!
+		<br/><br/>
+		Click our logo to begin!
+
+		</div>
 
 
 
